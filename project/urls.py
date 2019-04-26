8c4043488eb521a27558465d8709adba3ee77df5
Joey Leingang <multigl@gmail.com>
2019-04-25 22:40:08 -0400

    shell runs for python 2.7.15 and django 1.11

--- project/urls.py
+++ project/urls.py
@@ -1,15 +1,14 @@
-from django.conf.urls import *
+from django.conf.urls import url, include
 from django.contrib import admin
-from libscampi.contrib import cms 
+from libscampi.contrib.cms.sites import site
+from project import redirectors
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    (r'^comments/', include('django.contrib.comments.urls')),
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls)),
-    url(r'^tv-schedule-forward/(?P<key>\w+)/$', 'project.redirectors.tv_channel_schedule', name="channel-today"),
-    url(r'^tv-episode-forward/(?P<id>\w+)/$', 'project.redirectors.tv_episode_detail', name="episode-detail"),
-    (r'', include(cms.site.urls))  
-)
+urlpatterns = [
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^tv-schedule-forward/(?P<key>\w+)/$', redirectors.tv_channel_schedule, name="channel-today"),
+    url(r'^tv-episode-forward/(?P<id>\w+)/$', redirectors.tv_episode_detail, name="episode-detail"),
+    url(r'', include(site.urls))
+]
